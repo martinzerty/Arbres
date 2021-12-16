57f87c609ec48693ef80d7c38682418ac5f47c31
Martin Dumont <64594930+MartinDmont@users.noreply.github.com>
2021-12-16 21:30:20 +0100

    Update prefixe.py

--- prefixe.py
+++ prefixe.py
@@ -4,3 +4,17 @@ def prefixe(a):
         prefixe(a.enfant_gauche)
     if a.enfant_droit:
         prefixe(a.enfant_droit)
+
+def infixe(a):
+    if a.enfant_gauche:
+        infixe(a.enfant_gauche)
+    print(a.valeur)
+    if a.enfant_droit:
+        infixe(a.enfant_droit)
+
+def sufixe(a):
+    if a.enfant_gauche:
+        sufixe(a.enfant_gauche)
+    if a.enfant_droit:
+        sufixe(a.enfant_droit)
+    print(a.valeur)
