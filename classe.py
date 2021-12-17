85d1a33d5234387228ba18d06c48b83ba09874ba
Martin Dumont <64594930+MartinDmont@users.noreply.github.com>
2021-12-17 08:24:19 +0100

    Update classe.py

--- classe.py
+++ classe.py
@@ -1,6 +1,19 @@
 class Noeud:
     def __init__(self, valeur):
-    self.valeur = valeur
-    self.enfant_droit = None
-    self.enfant_gauche = None
-    
+        self.valeur = valeur
+        self.enfant_droit = None
+        self.enfant_gauche = None
+    def insert(self, valeur):
+        arbre = self
+        while arbre:
+            x = arbre
+            if valeur == x.valeur:
+                break
+            elif valeur < x.valeur:
+                arbre = arbre.enfant_gauche
+            elif valeur > x.valeur:
+                arbre = arbre.enfant_droit
+        if valeur < x.valeur:
+            x.enfant_gauche = Noeud(valeur)
+        else:
+            x.enfant_droit = Noeud(valeur)
